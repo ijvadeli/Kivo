94a12e898388bf1dd00a751f8cb817c80c7a4143
ijvadeli <idhvandelindt@icloud.com>
2026-05-20 09:19:53 +0200

    Current version, remove const file to make it work

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,26 +1,34 @@
 import { useState } from "react";
 import "./main.css";
+import { open } from "@tauri-apps/plugin-dialog";
+
+// Open a dialog
+const file = await open({
+  multiple: false,
+  directory: true,
+});
+
+const [val, setVal] = useState("");
+
+const clearField = () => {
+  setVal("");
+};
+
+const handleDownload = () => {
+  const blob = new Blob([val], { type: "text/plain" });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement("a");
+
+  link.href = url;
+  link.download = "test.txt";
+  document.body.appendChild(link);
+  link.click();
+
+  document.body.removeChild(link);
+  URL.revokeObjectURL(url);
+};
 
 export default function Main() {
-  const [val, setVal] = useState('')
-
-  const clearField = () => {
-    setVal("")
-  }
-
-  const handleDownload = () => {
-    const blob = new Blob([val],{type:'text/plain'})
-    const url = URL.createObjectURL(blob)
-    const link = document.createElement('a')
-
-    link.href = url
-    link.download = "test.txt"
-    document.body.appendChild(link)
-    link.click()
-
-    document.body.removeChild(link)
-    URL.revokeObjectURL(url)
-  }
   return (
     <div id="input-container">
       <div id="buttons">
@@ -29,7 +37,11 @@ export default function Main() {
         <button>Preview</button>
         <button>Settings</button>
       </div>
-      <textarea id="input-field" value={val} onChange={(e)=>setVal(e.target.value)}/>
+      <textarea
+        id="input-field"
+        value={val}
+        onChange={(e) => setVal(e.target.value)}
+      />
     </div>
   );
 }
